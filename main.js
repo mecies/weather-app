1710aea0608fbf05c5b08a0de5cda86a29770c14
unknown <maciekhnat@gmail.com>
2018-12-10 22:00:07 +0100

    bug fix 2

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const timer = require('./src/timer')
 const variables = require('./src/variables');
 const todayWeather = require('./src/todayWeather');
-const weatherFoecast = require('./src/forecastWeather');
+const weatherForecast = require('./src/forecastWeather');
 
 timer();
 document.getElementById('search').addEventListener('click', todayWeather);
-document.getElementById('search').addEventListener('click', weatherFoecast);
+document.getElementById('search').addEventListener('click', weatherForecast);
 document.getElementById('city').addEventListener('keypress', function(e){
     let key = e.which || e.keyCode;
     if (key === 13){
        todayWeather();
-       weatherFoecast();
+       weatherForecast();
     }
     else{
         return
