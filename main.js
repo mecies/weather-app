5976b0cb8d5768ba095569e8185021acac435d18
unknown <maciekhnat@gmail.com>
2018-12-10 20:45:20 +0100

    test commit

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ const todayWeather = require('./src/todayWeather');
 const weatherFoecast = require('./src/forecastWeather');
 
 timer();
-
-console.log(variables.url)
-
-//search city
 document.getElementById('search').addEventListener('click', todayWeather);
 document.getElementById('search').addEventListener('click', weatherFoecast);
 document.getElementById('city').addEventListener('keypress', function(e){
