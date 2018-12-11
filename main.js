26b9eddd8f7b0a4877ce66897e68ec6045a7c592
unknown <maciekhnat@gmail.com>
2018-12-11 11:03:36 +0100

    more code refactoring

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 const timer = require('./src/timer')
 const variables = require('./src/variables');
-const todayWeather = require('./src/todayWeather');
-const forecastWeather = require('./src/forecastWeather');
+const todayWeather = require('./src/weatherFunctions');
+const forecastWeather = require('./src/weatherFunctions');
 
 timer();
-document.getElementById('search').addEventListener('click', todayWeather);
-document.getElementById('search').addEventListener('click', forecastWeather);
-document.getElementById('city').addEventListener('keypress', function(e){
-    let key = e.which || e.keyCode;
-    if (key === 13){
+
+document.getElementById('search').addEventListener('submit', function(e){
+    e.preventDefault();
     todayWeather();
     forecastWeather();
-    }
-    else{
-        return
-    }
 });
 
 
+// let key = e.which || e.keyCode;
+// if (key === 13) {
+//     todayWeather();
+//     forecastWeather();
+// }
+// else {
+//     return
+// }
